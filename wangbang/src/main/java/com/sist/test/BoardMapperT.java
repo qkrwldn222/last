9d1ab271c8958f02a2f16c66736f88107bb8a55c
sist201 <sist201@DESKTOP-LL2EH9J>
2019-10-10 16:26:37 +0900

    보드 연결 수정

--- wangbang/src/main/java/com/sist/test/BoardMapperT.java
+++ wangbang/src/main/java/com/sist/test/BoardMapperT.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BoardMapper {
+//@Repository
+public interface BoardMapperT {
 	@Select("SELECT * FROM s_board")
 	public List<BoardVO> s_boardAllData();
 }
