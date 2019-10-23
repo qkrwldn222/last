58d4fb27537d51a47e30b76e22947e3109025244
sist201 <sist201@DESKTOP-LL2EH9J>
2019-10-23 14:26:04 +0900

    kkh brepl

---
+++ wangbang/src/main/java/com/sist/vo/BreplyVO.java
@@ -0,0 +1,19 @@
+package com.sist.vo;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BreplyVO {
+	private int rno;
+	private int bno;
+	private String rcontent;
+	private Date rregdate;
+	private String rdbday;
+	private String rid;
+	
+	
+}
